6dc68ddd2e840d4d9517270054f262c4d1462cf1
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-12-28 19:29:17 +0530

    files changed

---
+++ episode 4- talk is cheap show me the code/App.js
@@ -0,0 +1,65 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="app-logo">
+        <img
+          className="logo"
+          src="https://png.pngtree.com/png-clipart/20200727/original/pngtree-restaurant-logo-design-vector-template-png-image_5441058.jpg"
+          alt=""
+        />
+        {/* <h1 className="logo">LOGO</h1> */}
+      </div>
+      <div className="links">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+const RestaurantCard = () => {
+  return (
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0f0" }}>
+      <img
+        className="res-logo"
+        src="https://imgstaticcontent.lbb.in/lbbnew/wp-content/uploads/2018/03/08174730/Swiggy.jpg"
+        alt="res-logo"
+      />
+      <h3> PB7 foods</h3>
+      <h4>Indian,Pizza</h4>
+      <h4>4.7 stars</h4>
+      <h4>30 minutes</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <>
+      <div className="body">
+        <div className="search">search</div>
+        <div className="restaurant-container">
+          <RestaurantCard />
+        </div>
+      </div>
+    </>
+  );
+};
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Layout />);

ea955ea78d0a7695d2f37a1bed26fc3582e7be26
pallab17 <91554568+pallab17@users.noreply.github.com>
2024-01-14 00:02:24 +0530

    added props i.e dynamic data dynamic is changing data

--- episode 4- talk is cheap show me the code/App.js
+++ episode 4- talk is cheap show me the code/App.js
@@ -24,17 +24,45 @@ const Header = () => {
   );
 };
 const RestaurantCard = (props) => {
-  console.log(props); // prop ta hocche object ta jeita react pathache ei component e sob kota prop ke ekjayega ene wrap up kore
-  // const {resName,cuisine} = prop;    
+  console.log(props); // props ta hocche object ta jeita react pathache ei component e sob kota properties ke ekjayega ene wrap up kore
+  // const {resName,cuisine} = props;    
    // destructure kore enechi jate direct 
   //  <h3>{resName}</h3>
   // <h4>{cuisine}</h4>
   //  erom bhabe likhte pari
 
   /*
+  imppp 
 
-  
+   resData = {resObj}
+   ekhane resObj hocche ekta object
+   eibaree props tao hocche ekta object 
+
+   ebar amra jani je props r bhetore sob kota properties asche 
+   so resObj ta asche props r bhetore
+   so it becomes object of objects as props r bhetore resObj eseche bole
+
+   now to access a value example name without destrucing we will have to write
+   props.resData.data.name 
+
+   reason -->
+   as name ta hocche property ta  jeita data value r under e ache 
+   eibar ei data value along with type and subtype resObj r bhetore ache 
+   now we know that access korar jonno amra sudhu naam ta use kori property r not the content so amra bolbo
+   ei data value along with type and subtype resData r bhetore ache 
+   eibar ei resData ta props r bhetore jehetu ache tai amra
+   props.resData.data.name  eita likhe access korbo
+
+   with destructing it becomes
+    const {resData} = props;   --> destructure korlam mane props r bhetore jeita ache setake break kore lekha
+    resData.data.name --> access syntax
+
+
   */
+
+
+
+    const {resData} = props;
 
 
   
@@ -63,7 +91,8 @@ const {resName,cuisine} = prop; --< writing this
 {/* destructing korei likhte better lagche amar apatoto
  */}
       {/* destructure korchi bole resName directly likhe data take show korte paarchi */}
-      <h3>{props.resData.data.name} </h3>   
+      {/* <h3>{props.resData.data.name} </h3>    */}
+      <h3>{resData.data.name} </h3>  
       <h4>Indian</h4>
       <h4>4.7 stars</h4>
       <h4>30 minutes</h4>

0a565f10c7e4ccb5ec82709a54fa57171bf67917
pallab17 <91554568+pallab17@users.noreply.github.com>
2024-01-13 21:49:46 +0530

    added props i.e dynamic data dynamic is changing data

--- episode 4- talk is cheap show me the code/App.js
+++ episode 4- talk is cheap show me the code/App.js
@@ -23,7 +23,8 @@ const Header = () => {
     </div>
   );
 };
-const RestaurantCard = () => {
+const RestaurantCard = (prop) => {
+  const {resName,cuisine} = prop;
   return (
     <div className="res-card" style={{ backgroundColor: "#f0f0f0f0" }}>
       <img
@@ -31,8 +32,12 @@ const RestaurantCard = () => {
         src="https://imgstaticcontent.lbb.in/lbbnew/wp-content/uploads/2018/03/08174730/Swiggy.jpg"
         alt="res-logo"
       />
-      <h3> PB7 foods</h3>
+      {/* <h3> PB7 foods</h3>
       <h4>Indian,Pizza</h4>
+      <h4>4.7 stars</h4>
+      <h4>30 minutes</h4> */}
+      <h3>{resName}</h3>
+      <h4>{cuisine}</h4>
       <h4>4.7 stars</h4>
       <h4>30 minutes</h4>
     </div>
@@ -45,21 +50,12 @@ const Body = () => {
       <div className="body">
         <div className="search">search</div>
         <div className="restaurant-container">
+          <RestaurantCard 
+            resName = "PB7 foods"   // prop eita 
+            cuisine = "Indian,Pizza"  // react ei sobkota 
+          />
           <RestaurantCard />
-          <RestaurantCard />
-          <RestaurantCard />
-          <RestaurantCard />
-          <RestaurantCard />
-          <RestaurantCard />
-          <RestaurantCard />
-          <RestaurantCard />
-          <RestaurantCard />
-          <RestaurantCard />
-          <RestaurantCard />
-          <RestaurantCard />
-          <RestaurantCard />
-          <RestaurantCard />
-          <RestaurantCard />
+          
         </div>
       </div>
     </>
@@ -77,3 +73,8 @@ const Layout = () => {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<Layout />);
+   
+
+
+// passsing a prop to a component is same like passing arguments/parameter to a fn
+// prop = properties = parameters that we pass to commponents

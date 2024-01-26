a3ba6cec5cd3eafd6afed1737f0d8acbbf94c7df
pallab17 <91554568+pallab17@users.noreply.github.com>
2024-01-26 20:53:34 +0530

    lets get hook started

--- episode 5 -  lets get hooked/src/common/constants.js
+++ episode 5 -  lets get hooked/src/common/constants.js
@@ -3,10 +3,15 @@
 
 // variable name guno capital plus underscore e likhte hobe 
 
-const LOGO_URL = "https://png.pngtree.com/png-clipart/20200727/original/pngtree-restaurant-logo-design-vector-template-png-image_5441058.jpg";
+ export const LOGO_URL = "https://png.pngtree.com/png-clipart/20200727/original/pngtree-restaurant-logo-design-vector-template-png-image_5441058.jpg";
 
-const CDN_URL =  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/";
+export const CDN_URL =  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/";
 
 
 //  utils folder r naam ami common folder use korchi for my good
 // util/common folder r content gulo use korbe everyone in the code oijonnoi common mal guno ei folder r bhetore thakbe
+
+
+// there are 2 types of export
+// default export
+//  and multiple jinis jokhun same file theke export hobe tokhun we use name-export method

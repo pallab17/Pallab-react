cdf448180346cad712c76a00c6b8f59305e372ea
pallab17 <91554568+pallab17@users.noreply.github.com>
2024-01-26 20:54:07 +0530

    lets get hook started

--- episode 5 -  lets get hooked/src/common/constants.js
+++ episode 5 -  lets get hooked/src/common/constants.js
@@ -3,7 +3,7 @@
 
 // variable name guno capital plus underscore e likhte hobe 
 
- export const LOGO_URL = "https://png.pngtree.com/png-clipart/20200727/original/pngtree-restaurant-logo-design-vector-template-png-image_5441058.jpg";
+export const LOGO_URL = "https://png.pngtree.com/png-clipart/20200727/original/pngtree-restaurant-logo-design-vector-template-png-image_5441058.jpg";
 
 export const CDN_URL =  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/";
 

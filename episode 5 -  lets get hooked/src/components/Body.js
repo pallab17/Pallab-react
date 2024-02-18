be574700023fc376ee4d1280c34f76ae445e1bd7
pallab17 <91554568+pallab17@users.noreply.github.com>
2024-02-18 17:19:28 +0530

    lets get hook started

--- episode 5 -  lets get hooked/src/components/Body.js
+++ episode 5 -  lets get hooked/src/components/Body.js
@@ -1,46 +1,87 @@
+import {useState} from "react";
 import RestaurantCard from "./RestaurantCard";
 import resList from "../common/mockdata";
 
+
 const Body = () => {
+
+  const[listOfRestaurants,setListOfRestaurants] = useState([{
+    type: 'restaurant',
+    data: {
+      id: '334475',
+      name: 'KFC',
+      cloudinaryImageId: 'bdcd233971b7c81bf77e1fa4471280eb',
+      cuisines: ['Burgers', 'Biryani', 'American', 'Snacks', 'Fast Food'],
+      costForTwo: 40000,
+      deliveryTime: 36,
+      avgRating: '3.8',
+    },
+  },
+  {
+    type: 'restaurant',
+    data: {
+      id: '334476',
+      name: 'Dominos',
+      cloudinaryImageId: 'bdcd233971b7c81bf77e1fa4471280eb',
+      cuisines: ['Burgers', 'Biryani', 'American', 'Snacks', 'Fast Food'],
+      costForTwo: 40000,
+      deliveryTime: 36,
+      avgRating: '4.8',
+    },
+  },
+  {
+    type: 'restaurant',
+    data: {
+      id: '334477',
+      name: 'McDonals',
+      cloudinaryImageId: 'bdcd233971b7c81bf77e1fa4471280eb',
+      cuisines: ['Burgers', 'Biryani', 'American', 'Snacks', 'Fast Food'],
+      costForTwo: 40000,
+      deliveryTime: 36,
+      avgRating: '4.2',
+    },
+  }
+]);
+
   // swiggy r whole api use na kore just 2-3 te use korchi for understanding hooks
-  const listOfRestaurants = [
-    {
-      type: 'restaurant',
-      data: {
-        id: '334475',
-        name: 'KFC',
-        cloudinaryImageId: 'bdcd233971b7c81bf77e1fa4471280eb',
-        cuisines: ['Burgers', 'Biryani', 'American', 'Snacks', 'Fast Food'],
-        costForTwo: 40000,
-        deliveryTime: 36,
-        avgRating: '3.8',
-      },
-    },
-    {
-      type: 'restaurant',
-      data: {
-        id: '334476',
-        name: 'Dominos',
-        cloudinaryImageId: 'bdcd233971b7c81bf77e1fa4471280eb',
-        cuisines: ['Burgers', 'Biryani', 'American', 'Snacks', 'Fast Food'],
-        costForTwo: 40000,
-        deliveryTime: 36,
-        avgRating: '4.8',
-      },
-    },
-    {
-      type: 'restaurant',
-      data: {
-        id: '334477',
-        name: 'McDonals',
-        cloudinaryImageId: 'bdcd233971b7c81bf77e1fa4471280eb',
-        cuisines: ['Burgers', 'Biryani', 'American', 'Snacks', 'Fast Food'],
-        costForTwo: 40000,
-        deliveryTime: 36,
-        avgRating: '4.2',
-      },
-    },
-  ];
+  // let listOfRestaurants = [
+  //   {
+  //     type: 'restaurant',
+  //     data: {
+  //       id: '334475',
+  //       name: 'KFC',
+  //       cloudinaryImageId: 'bdcd233971b7c81bf77e1fa4471280eb',
+  //       cuisines: ['Burgers', 'Biryani', 'American', 'Snacks', 'Fast Food'],
+  //       costForTwo: 40000,
+  //       deliveryTime: 36,
+  //       avgRating: '3.8',
+  //     },
+  //   },
+  //   {
+  //     type: 'restaurant',
+  //     data: {
+  //       id: '334476',
+  //       name: 'Dominos',
+  //       cloudinaryImageId: 'bdcd233971b7c81bf77e1fa4471280eb',
+  //       cuisines: ['Burgers', 'Biryani', 'American', 'Snacks', 'Fast Food'],
+  //       costForTwo: 40000,
+  //       deliveryTime: 36,
+  //       avgRating: '4.8',
+  //     },
+  //   },
+  //   {
+  //     type: 'restaurant',
+  //     data: {
+  //       id: '334477',
+  //       name: 'McDonals',
+  //       cloudinaryImageId: 'bdcd233971b7c81bf77e1fa4471280eb',
+  //       cuisines: ['Burgers', 'Biryani', 'American', 'Snacks', 'Fast Food'],
+  //       costForTwo: 40000,
+  //       deliveryTime: 36,
+  //       avgRating: '4.2',
+  //     },
+  //   },
+  // ];
 
   return (
     <>
@@ -52,7 +93,12 @@ const Body = () => {
             onClick={() => {
               // ekhane logic ta amake bhabte + likhte hobe 
               // logic will contain--> je bhai button ta click korle only top rated restaurant screen e dekhabe
-              console.log("button clicked");
+              //console.log("button clicked");
+              // listOfRestaurants = listOfRestaurants.filter(
+              //   (r)=>r.data.avgRating>4
+              // );
+              // console.log(listOfRestaurants);
+              
             }}
           >
             Top Rated Restaurants

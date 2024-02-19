370806c279dad24a128201aaf5349174f971c895
pallab17 <91554568+pallab17@users.noreply.github.com>
2024-02-19 21:48:50 +0530

    lets get hook started

--- episode 6 -  explore the world/src/components/Body.js
+++ episode 6 -  explore the world/src/components/Body.js
@@ -1,108 +1,27 @@
-import {useState} from "react";
+import {useState,useEffect} from "react";
 import RestaurantCard from "./RestaurantCard";
 import resList from "../common/mockdata";
 
 
 const Body = () => {
 
-  // method 1  - destructured in a single line
-  // const[listOfRestaurants,setListOfRestaurants] = useState(resList);   eita ke onno bahbe o ekha jaye in simple terms 
+  
+   const[listOfRestaurants,setListOfRestaurants] = useState(resList);   
 
-  // method 2 
-  //  useState() fn ta jehetu ekta array arr provide korche amader so we can write 
-  const arr = useState(resList);
-  // now this array arr contains 2 elements listOfRestaurants,setListOfRestaurants
-  // i.e
-   const listOfRestaurants = arr[0];
-   const setListOfRestaurants = arr[1];
+  /* useEffect sikhchi 
+  useeffecr hook - useeffect();
+  import from react as a named import
+  useEffect
 
-  //  method 3  
-    //  useState() fn ta jehetu ekta array arr provide korche amader so we can write 
-  // const arr = useState(resList);
-  // now this array arr contains 2 elements listOfRestaurants,setListOfRestaurants
-   // const[listOfRestaurants,setListOfRestaurants] = arr;   // destructuring here
-   
+
+  */
+ useEffect(()=>{
+
+ },[]);
 
 
 
-//   const[listOfRestaurants,setListOfRestaurants] = useState([{
-//     type: 'restaurant',
-//     data: {
-//       id: '334475',
-//       name: 'KFC',
-//       cloudinaryImageId: 'bdcd233971b7c81bf77e1fa4471280eb',
-//       cuisines: ['Burgers', 'Biryani', 'American', 'Snacks', 'Fast Food'],
-//       costForTwo: 40000,
-//       deliveryTime: 36,
-//       avgRating: '3.8',
-//     },
-//   },
-//   {
-//     type: 'restaurant',
-//     data: {
-//       id: '334476',
-//       name: 'Dominos',
-//       cloudinaryImageId: 'bdcd233971b7c81bf77e1fa4471280eb',
-//       cuisines: ['Burgers', 'Biryani', 'American', 'Snacks', 'Fast Food'],
-//       costForTwo: 40000,
-//       deliveryTime: 36,
-//       avgRating: '4.8',
-//     },
-//   },
-//   {
-//     type: 'restaurant',
-//     data: {
-//       id: '334477',
-//       name: 'McDonals',
-//       cloudinaryImageId: 'bdcd233971b7c81bf77e1fa4471280eb',
-//       cuisines: ['Burgers', 'Biryani', 'American', 'Snacks', 'Fast Food'],
-//       costForTwo: 40000,
-//       deliveryTime: 36,
-//       avgRating: '4.2',
-//     },
-//   }
-// ]);
-
-  // swiggy r whole api use na kore just 2-3 te use korchi for understanding hooks
-  // let listOfRestaurants = [
-  //   {
-  //     type: 'restaurant',
-  //     data: {
-  //       id: '334475',
-  //       name: 'KFC',
-  //       cloudinaryImageId: 'bdcd233971b7c81bf77e1fa4471280eb',
-  //       cuisines: ['Burgers', 'Biryani', 'American', 'Snacks', 'Fast Food'],
-  //       costForTwo: 40000,
-  //       deliveryTime: 36,
-  //       avgRating: '3.8',
-  //     },
-  //   },
-  //   {
-  //     type: 'restaurant',
-  //     data: {
-  //       id: '334476',
-  //       name: 'Dominos',
-  //       cloudinaryImageId: 'bdcd233971b7c81bf77e1fa4471280eb',
-  //       cuisines: ['Burgers', 'Biryani', 'American', 'Snacks', 'Fast Food'],
-  //       costForTwo: 40000,
-  //       deliveryTime: 36,
-  //       avgRating: '4.8',
-  //     },
-  //   },
-  //   {
-  //     type: 'restaurant',
-  //     data: {
-  //       id: '334477',
-  //       name: 'McDonals',
-  //       cloudinaryImageId: 'bdcd233971b7c81bf77e1fa4471280eb',
-  //       cuisines: ['Burgers', 'Biryani', 'American', 'Snacks', 'Fast Food'],
-  //       costForTwo: 40000,
-  //       deliveryTime: 36,
-  //       avgRating: '4.2',
-  //     },
-  //   },
-  // ];
-
+  
   return (
     <>
       <div className="body">

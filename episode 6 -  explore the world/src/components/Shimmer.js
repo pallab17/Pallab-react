1b5be95a304fae4fadc265dbe319e95dce92619f
pallab17 <91554568+pallab17@users.noreply.github.com>
2024-02-21 11:33:27 +0530

    lets get hook started

---
+++ episode 6 -  explore the world/src/components/Shimmer.js
@@ -0,0 +1,20 @@
+const Shimmer = ()=>{
+    return (
+        <div className="Shimmer-baksho">
+        <div className="Shimmer-card"></div>
+        <div className="Shimmer-card"></div>
+        <div className="Shimmer-card"></div>
+        <div className="Shimmer-card"></div>
+        <div className="Shimmer-card"></div>
+        <div className="Shimmer-card"></div>
+        <div className="Shimmer-card"></div>
+        <div className="Shimmer-card"></div>
+        <div className="Shimmer-card"></div>
+        <div className="Shimmer-card"></div>
+        <div className="Shimmer-card"></div>
+        <div className="Shimmer-card"></div>
+        <div className="Shimmer-card"></div>
+
+        </div>
+    )
+}

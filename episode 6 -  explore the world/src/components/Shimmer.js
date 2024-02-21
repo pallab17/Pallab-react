ae008ce2512945d4c42322d26f67d5f0064955b5
pallab17 <91554568+pallab17@users.noreply.github.com>
2024-02-21 11:57:40 +0530

    lets get hook started

--- episode 6 -  explore the world/src/components/Shimmer.js
+++ episode 6 -  explore the world/src/components/Shimmer.js
@@ -1,16 +1,16 @@
 const Shimmer = ()=> {
     return(
         <div className="shimmer-container">
-            <div className="shimmer-card">card1</div>
-            <div className="shimmer-card">card1</div>
-            <div className="shimmer-card">card1</div>
-            <div className="shimmer-card">card1</div>
-            <div className="shimmer-card">card1</div>
-            <div className="shimmer-card">card1</div>
-            <div className="shimmer-card">card1</div>
-            <div className="shimmer-card">card1</div>
-            <div className="shimmer-card">card1</div>
-            <div className="shimmer-card">card1</div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
         
         </div>
     )

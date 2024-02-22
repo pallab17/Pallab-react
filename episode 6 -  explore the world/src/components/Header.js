2d4606781b27320bda7cc4b99935a8afd00722bc
pallab17 <91554568+pallab17@users.noreply.github.com>
2024-02-22 09:00:58 +0530

    lets get hook started

--- episode 6 -  explore the world/src/components/Header.js
+++ episode 6 -  explore the world/src/components/Header.js
@@ -1,11 +1,9 @@
 import { LOGO_URL } from "../common/constants";
 import { useState } from "react";
-// import {Component} from path --> syntax for named-export 
-
+// import {Component} from path --> syntax for named-export
 
 const Header = () => {
-
-  const[loginbutton,setloginbutton]=useState("Login");
+  const [loginbutton, setloginbutton] = useState("Login");
   /*
   jokhun ei ami login button ta click korchi tokhun setloginbutton fn ta loginbutton r  value take logout e change korche 
   and jokhun ei state variable r function ta call hocche , value update hocche tokhun ei react ei gota component take render(show/present/user ke screen dekhano) korche with updated values.
@@ -16,31 +14,33 @@ const Header = () => {
   and tarpor re-render hoye ei duplicate set variable ta with updated value.
   */
 
-
-    return (
-      <div className="header">
-        <div className="app-logo">
-          <img
-            className="logo"
-            src={LOGO_URL}
-            alt=""
-          />
-          {/* <h1 className="logo">LOGO</h1> */}
-        </div>
-        <div className="links">
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-            <li>Cart</li>
-            <button className="but" onClick={()=>{
-              setloginbutton("Logout");
-             }}>{loginbutton}</button>
-          </ul>
-        </div>
+  return (
+    <div className="header">
+      <div className="app-logo">
+        <img className="logo" src={LOGO_URL} alt="" />
+        {/* <h1 className="logo">LOGO</h1> */}
       </div>
-    );
-  };
+      <div className="links">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+          <button
+            className="but"
+            onClick={() => {
+              loginbutton === "Login"
+                ? setloginbutton("Logout")
+                : setloginbutton("Login");
+            }}
+          >
+            {loginbutton}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 //   export default componentname --> syntax
 

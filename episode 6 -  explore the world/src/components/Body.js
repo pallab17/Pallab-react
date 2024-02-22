5c29942c53ffce46f87a902db515e318b9f0a855
pallab17 <91554568+pallab17@users.noreply.github.com>
2024-02-22 09:53:55 +0530

    lets get hook started

--- episode 6 -  explore the world/src/components/Body.js
+++ episode 6 -  explore the world/src/components/Body.js
@@ -1,13 +1,11 @@
-import {useState,useEffect} from "react";
+import { useState, useEffect } from "react";
 import RestaurantCard from "./RestaurantCard";
-import Shimmer from "./Shimmer"
+import Shimmer from "./Shimmer";
 //import resList from "../common/mockdata";
 
-
 const Body = () => {
-
   //const[listOfRestaurants,setListOfRestaurants] = useState(resList); we are not using the mock data here as swiggy nijeder api data format change koreche so logic break hocche
-   const[listOfRestaurants,setListOfRestaurants] = useState([]);   // prothome our app doesnt have any web page
+  const [listOfRestaurants, setListOfRestaurants] = useState([]); // prothome our app doesnt have any web page
 
   /* useEffect sikhchi 
   useeffecr hook - useeffect();
@@ -19,34 +17,34 @@ const Body = () => {
 
 
   */
- useEffect(()=>{
-  fetchData(); // ekta fn banachi jeitar modhe logic likhbo to fetch live data from swiggy
+  useEffect(() => {
+    fetchData(); // ekta fn banachi jeitar modhe logic likhbo to fetch live data from swiggy
+  }, []);
 
- },[]);
+  //  const fetchData = () => {
+  //   const data = fetch(
+  //     "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.51800&lng=88.38320&collection=83647&tags=layout_CCS_Chinese&sortBy=&filters=&type=rcv2&offset=0&page_type=null"
+  //   );
+  //   // fetch(); ei fetch fn ta data fetch kore anbe from api - swiggy r api
+  //   // and it will return a promise
+  //   // so we have to resolve a promise
+  //   // method 1 traditional - then method with catch method to handle errors
+  //   // method 2 - industry practice - async await function
+  //  }
 
-//  const fetchData = () => {
-//   const data = fetch(
-//     "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.51800&lng=88.38320&collection=83647&tags=layout_CCS_Chinese&sortBy=&filters=&type=rcv2&offset=0&page_type=null"
-//   );
-//   // fetch(); ei fetch fn ta data fetch kore anbe from api - swiggy r api
-//   // and it will return a promise
-//   // so we have to resolve a promise
-//   // method 1 traditional - then method with catch method to handle errors
-//   // method 2 - industry practice - async await function 
-//  }
-
-   const fetchData = async() => {
+  const fetchData = async () => {
     const data = await fetch(
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING#"
-    )
-// converting this data to json
-const json = await data.json();
+    );
+    // converting this data to json
+    const json = await data.json();
 
-console.log(json);
-// eibar ami chaichi ei json r modhe new restuarant r data r list ke take updated data of listofrestaurants baniye di
-setListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-
-   }
+    console.log(json);
+    // eibar ami chaichi ei json r modhe new restuarant r data r list ke take updated data of listofrestaurants baniye di
+    setListOfRestaurants(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
 
   //  if(listOfRestaurants.length === 0){
   //   return <h1>loading...</h1>;
@@ -59,17 +57,22 @@ setListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithSty
   // }
 
   // using ternary operator
-  return listOfRestaurants.length===0?(
-    <Shimmer/>
-  ):(
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <>
       <div className="body">
         {/* <div className="search">search</div> */}
         <div className="filter">
+        <div className="search">
+        <input type="text" className="search-box" />
+        <button>Search</button>
+        
+        </div>
           <button
             className="filter-btn"
             onClick={() => {
-              // ekhane logic ta amake bhabte + likhte hobe 
+              // ekhane logic ta amake bhabte + likhte hobe
               // logic will contain--> je bhai button ta click korle only top rated restaurant screen e dekhabe
               //console.log("button clicked");
               // listOfRestaurants = listOfRestaurants.filter(
@@ -77,30 +80,24 @@ setListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithSty
               // );
               // console.log(listOfRestaurants);
               const filteredList = listOfRestaurants.filter(
-                (r)=>r.info.avgRating>4.2
-                // filteredlist bole ekta var banachi jeitar modhe ami listofRestaurants take update korchi according to their avg rating 
-                
+                (r) => r.info.avgRating > 4.2
+                // filteredlist bole ekta var banachi jeitar modhe ami listofRestaurants take update korchi according to their avg rating
               );
               setListOfRestaurants(filteredList); // tarpor ei filteredlist takei pass kore dicchi setlistofrestaurants r modhe such that original list of restuarants ta update hoye jaye when the top rated restaurant button is clicked
-
             }}
           >
             Top Rated Restaurants
           </button>
           <button
-           className="filter-btn"
-           onClick={
-            ()=>{
+            className="filter-btn"
+            onClick={() => {
               const filteredList = listOfRestaurants.filter(
-                (r)=>r.info.sla.deliveryTime < 20
+                (r) => r.info.sla.deliveryTime < 20
               );
               setListOfRestaurants(filteredList);
-
-            }
-           }
+            }}
           >
-          Fast deliveryTime
-            
+            Fast deliveryTime
           </button>
         </div>
         <div className="restaurant-container">

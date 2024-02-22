12d8dd8338be06c68eb0c927cc00bef87512c401
pallab17 <91554568+pallab17@users.noreply.github.com>
2024-02-22 08:55:51 +0530

    lets get hook started

--- episode 6 -  explore the world/src/components/Header.js
+++ episode 6 -  explore the world/src/components/Header.js
@@ -6,6 +6,15 @@ import { useState } from "react";
 const Header = () => {
 
   const[loginbutton,setloginbutton]=useState("Login");
+  /*
+  jokhun ei ami login button ta click korchi tokhun setloginbutton fn ta loginbutton r  value take logout e change korche 
+  and jokhun ei state variable r function ta call hocche , value update hocche tokhun ei react ei gota component take render(show/present/user ke screen dekhano) korche with updated values.
+
+
+  but question ta hocche loginbutton ta toh const type r tahole update hocche ki kore ?
+  ans ta hooche je jokhun setvariable r function ta call hocche tokhun ei aar ekta instance/new/copy/duplicate set variable with same-name create hoye but with updated values
+  and tarpor re-render hoye ei duplicate set variable ta with updated value.
+  */
 
 
     return (

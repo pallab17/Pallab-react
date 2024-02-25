e745cba933d22e962e3453fec9a9faf2982ad34b
pallab17 <91554568+pallab17@users.noreply.github.com>
2024-02-25 17:39:13 +0530

    learning react-router-dom

--- episode 7 - finding the path/src/components/Error.js
+++ episode 7 - finding the path/src/components/Error.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useRouteError } from 'react-router-dom'
 
 const Error = () => {
+    const err = useRouteError();
+    console.log(err);
   return (
     <div>
     <h1>Oops....</h1>

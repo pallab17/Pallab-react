c66699e2c9e13dd67282f958433323b69e8ceb44
pallab17 <91554568+pallab17@users.noreply.github.com>
2024-02-25 17:27:06 +0530

    learning react-router-dom

--- episode 7 - finding the path/src/App.js
+++ episode 7 - finding the path/src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Body from "./components/Body";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Error from "./components/Error";
 import { createBrowserRouter , RouterProvider } from "react-router-dom";
 
 

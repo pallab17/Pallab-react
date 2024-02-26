a7bbc02990de8e6ced2cea3020fa43b9748d2ea7
pallab17 <91554568+pallab17@users.noreply.github.com>
2024-02-27 00:49:46 +0530

    learning react-router-dom

--- episode 7 - finding the path/src/components/RestaurantMenu.js
+++ episode 7 - finding the path/src/components/RestaurantMenu.js
@@ -35,10 +35,15 @@ const RestaurantMenu = () => {
       <h2>{cuisines.join(", ")}</h2>
       <h3>{costForTwoMessage}</h3>
       <ul>
-        {}
-        <li>{itemCards[0].card.info.name}</li>
+        {itemCards.map((p)=>(
+          <li>
+            {p.card.info.name} - {" Rs."}
+            {p.card.info.price/100}
+          </li>
+        ))}
+        {/* <li>{itemCards[0].card.info.name}</li>
         <li>{itemCards[1].card.info.name}</li>
-        <li>{itemCards[2].card.info.name}</li>
+        <li>{itemCards[2].card.info.name}</li> */}
         
       </ul>
     </div>

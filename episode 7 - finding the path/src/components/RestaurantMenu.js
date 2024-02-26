9443af564b9f147d3ea215ebadd7798559410f9a
pallab17 <91554568+pallab17@users.noreply.github.com>
2024-02-26 18:16:05 +0530

    learning react-router-dom

--- episode 7 - finding the path/src/components/RestaurantMenu.js
+++ episode 7 - finding the path/src/components/RestaurantMenu.js
@@ -3,27 +3,39 @@ import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 
 const RestaurantMenu = () => {
-  const [resInfo, setresinfo] = useState(null);
+  const [resInfo, setresInfo] = useState(null);
   useEffect(() => {
     fetchMenu();
-  }, []);
+  },[]);
 
   const fetchMenu = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=5934&catalog_qa=undefined&submitAction=ENTER"
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=5934&catalog_qa=undefined&submitAction=ENTER#"
     );
 
     const json = await data.json();
 
     console.log(json);
-    setresinfo(json.data);
+    setresInfo(json);
   };
-  return resInfo === NULL ? (
-    <Shimmer />
-  ) : (
+ 
+  if( resInfo === null ){
+    return <Shimmer/>;
+  }
+
+
+  const {name,cuisines,costForTwoMessage} = resInfo?.data?.cards[2]?.card?.card?.info;
+
+
+  const {itemCards} = resInfo.data.cards[4].groupedCard.cardGroupMap.REGULAR.cards[1].card.card.itemCards
+  
+    
+
+  return (
     <div className="menu">
-      <h1>name of rest</h1>
-      <h2>menu</h2>
+      <h1>{name}</h1>
+      <h2>{cuisines.join(", ")}</h2>
+      <h3>{costForTwoMessage}</h3>
       <ul>
         <li>momo</li>
         <li>chips</li>

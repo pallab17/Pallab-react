9bf4726e1075c060736286fc1d189eb73bc19843
pallab17 <91554568+pallab17@users.noreply.github.com>
2024-02-26 11:59:13 +0530

    learning react-router-dom

--- episode 7 - finding the path/src/components/Header.js
+++ episode 7 - finding the path/src/components/Header.js
@@ -60,6 +60,16 @@ const Header = () => {
 
             thats why it is called single page applications spa --> all the routing is just about component exchanging themselves 
              
+             ## Q: What is `SPA`?
+
+A: `Single Page Application (SPA)` is a web application that dynamically updates the webpage with data from web server without reloading/refreshing the entire page. All the HTML, CSS, JS are retrieved in the initial load and other data/resources can be loaded dynamically whenever required. An SPA is sometimes referred to as a `single-page interface (SPI)`.
+
+## Q: What is the difference between `Client Side Routing` and `Server Side Routing`?
+
+A: In `Server-side routing or rendering (SSR)`, every change in URL, http request is made to server to fetch the webpage, and replace the current webpage with the older one.
+
+In `Client-side routing or rendering (CSR)`, during the first load, the webapp is loaded from server to client, after which whenever there is a change in URL, the router library navigates the user to the new page without sending any request to backend. All `Single Page Applications uses client-side routing`.
+
              
              */}
           </li>

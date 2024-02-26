f0758b80fa122661ecc7f52c8829ac461e750943
pallab17 <91554568+pallab17@users.noreply.github.com>
2024-02-27 01:12:33 +0530

    learning react-router-dom

--- episode 7 - finding the path/src/common/constants.js
+++ episode 7 - finding the path/src/common/constants.js
@@ -7,6 +7,7 @@ export const LOGO_URL = "https://png.pngtree.com/png-clipart/20200727/original/p
 
 export const CDN_URL =  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/";
 
+export const MENU_API =       "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=";
 
 //  utils folder r naam ami common folder use korchi for my good
 // util/common folder r content gulo use korbe everyone in the code oijonnoi common mal guno ei folder r bhetore thakbe

2fa9e198ddc7e7062f70d5f77c39cc93af927391
pallab17 <91554568+pallab17@users.noreply.github.com>
2024-02-26 16:32:24 +0530

    learning react-router-dom

--- episode 7 - finding the path/src/components/RestaurantMenu.js
+++ episode 7 - finding the path/src/components/RestaurantMenu.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 const RestaurantMenu = () => {
   return (
-    <div>
-    
+    <div className='menu'>
+    <h1>name of rest</h1>
+    <h2>menu</h2>
+    <ul>
+        <li>momo</li>
+        <li>chips</li>
+        <li>biryani</li>
+    </ul>
+
       
     </div>
   )

83cfb72159da156159a0859f50120e65a8e4860c
pallab17 <91554568+pallab17@users.noreply.github.com>
2024-02-26 17:03:17 +0530

    learning react-router-dom

--- episode 7 - finding the path/src/components/RestaurantMenu.js
+++ episode 7 - finding the path/src/components/RestaurantMenu.js
@@ -1,34 +1,36 @@
-import React from 'react'
-import { useEffect } from 'react';
+import React from "react";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const RestaurantMenu = () => {
-    useEffect(()=>{
-        fetchMenu();
+  const [resInfo, setresinfo] = useState(null);
+  useEffect(() => {
+    fetchMenu();
+  }, []);
 
-    },[]);
+  const fetchMenu = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=5934&catalog_qa=undefined&submitAction=ENTER"
+    );
 
-    const fetchMenu = async ()=>{
-        const data =  await fetch();
-        
-        const json = await data.json();
+    const json = await data.json();
 
-        console.log(json);
-
-        
-    }
-  return (
-    <div className='menu'>
-    <h1>name of rest</h1>
-    <h2>menu</h2>
-    <ul>
+    console.log(json);
+    setresinfo(json.data);
+  };
+  return resInfo === NULL ? (
+    <Shimmer />
+  ) : (
+    <div className="menu">
+      <h1>name of rest</h1>
+      <h2>menu</h2>
+      <ul>
         <li>momo</li>
         <li>chips</li>
         <li>biryani</li>
-    </ul>
-
-      
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default RestaurantMenu
+export default RestaurantMenu;

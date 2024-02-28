53ba7c476b09505bbe3c0beabdf8991de8fc9604
pallab17 <91554568+pallab17@users.noreply.github.com>
2024-02-28 14:06:32 +0530

    learning react-router-dom

--- episode 7 - finding the path/src/components/RestaurantMenu.js
+++ episode 7 - finding the path/src/components/RestaurantMenu.js
@@ -25,7 +25,7 @@ const RestaurantMenu = () => {
 
   const fetchMenu = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=" +
+       +
         resID +
         "&catalog_qa=undefined&submitAction=ENTER"
 

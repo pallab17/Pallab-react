de4ad0704306e893b93cb6a5097fcc232592edab
pallab17 <91554568+pallab17@users.noreply.github.com>
2024-03-04 09:02:05 +0530

    lets get classy

---
+++ episode 8 - lets get classy/src/components/User.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const User = () => {
+  return (
+    <div>
+    <h2>Pallab  Banerjee</h2>
+    <h3>location - Bally</h3>
+    <h4>Contact - github-pallab17</h4>
+
+      
+    </div>
+  )
+}
+
+export default User;

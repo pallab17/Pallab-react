3d7d25744a40f43c776ddba36698186a350f963d
pallab17 <91554568+pallab17@users.noreply.github.com>
2024-03-04 09:19:15 +0530

    lets get classy

---
+++ episode 8 - lets get classy/src/components/UserClass.js
@@ -0,0 +1,14 @@
+/*
+class based component - normal js class
+
+how to create a class syntax
+class component_name
+e.g class UserClass
+
+and to make it class based component such that reacts understands that is a class based component and tracks it
+syntax entends React.Component
+*/
+
+class UserClass extends React.Component{
+    
+}

bdd1d8d310027e65baf66913d2f395d5c25961b2
pallab17 <91554568+pallab17@users.noreply.github.com>
2024-03-04 09:04:21 +0530

    lets get classy

--- episode 8 - lets get classy/src/components/About.js
+++ episode 8 - lets get classy/src/components/About.js
@@ -1,8 +1,11 @@
+import User from "./user";
+
 const About = ()=> {
     return(
         <>
             <h1>About Us page</h1>
             <h2>this is me pb7 watching react webseries by akshay saini</h2>
+            <User/>
         </>
     )
 }
--- episode 8 - lets get classy/src/components/User.js
+++ episode 8 - lets get classy/src/components/User.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const User = () => {
   return (
-    <div>
+    <div className='user-card'>
     <h2>Pallab  Banerjee</h2>
     <h3>location - Bally</h3>
     <h4>Contact - github-pallab17</h4>

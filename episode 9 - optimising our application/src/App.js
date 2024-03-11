f6b429bcc39c163628881ff3f46332b6d38a27b6
pallab17 <91554568+pallab17@users.noreply.github.com>
2024-03-12 03:00:03 +0530

    optimising our app

--- episode 9 - optimising our application/src/App.js
+++ episode 9 - optimising our application/src/App.js
@@ -6,6 +6,7 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import RestaurantMenu from "./components/RestaurantMenu";
 import Error from "./components/Error";
+import Grocery from "./components/Grocery";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 const currYear = new Date().getFullYear();
@@ -82,6 +83,10 @@ const appRouter = createBrowserRouter([
       {
         path: "/contact",
         element: <Contact />,
+      },
+      {
+        path: "/grocery",
+        element: <Grocery/>
       },
       {
         path: "/restaurants/:resID",

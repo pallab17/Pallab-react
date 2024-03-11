9e7420b2e12edbd28dda4c3f64cbc011ceee51ae
pallab17 <91554568+pallab17@users.noreply.github.com>
2024-03-11 20:05:34 +0530

    optimising our app

--- episode 9 - optimising our application/src/common/useOnlineStatus.js
+++ episode 9 - optimising our application/src/common/useOnlineStatus.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState,useEffect } from 'react';
 
+// Creating a function that will help us to. engage the user when the user is offline, then we will show them a screen where the user will be shown the other. four hundred four in her Internet page, such that the user knows that their Internet is off.
 const useOnlineStatus = () => {
   
     const [onlineStatus, setOnlineStatus] = useState(true);

f13af49bfda76874194797788716eebb491b09e8
pallab17 <91554568+pallab17@users.noreply.github.com>
2024-12-29 19:27:15 +0530

    cors

--- episode 12 - lets build our store/src/components/RestaurantMenu.js
+++ episode 12 - lets build our store/src/components/RestaurantMenu.js
@@ -69,7 +69,8 @@ const RestaurantMenu = () => {
     key={category?.card?.card.title}
      data={category?.card?.card}
       showItems={index===showIndex ? true : false}
-      setShowIndex={()=> setShowIndex(index)}
+      // setShowIndex={()=> setShowIndex(index)}
+      setShowIndex={() => setShowIndex(index === showIndex ? null : index)}
      />
    ))}
       
